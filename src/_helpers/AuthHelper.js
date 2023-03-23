c550966901fe66d6e8ab48ab8a3a9e9a7b5d311e
unknown <nzr.frz@gmail.com>
2023-03-23 13:01:20 +0700

    change status response

--- src/_helpers/AuthHelper.js
+++ src/_helpers/AuthHelper.js
@@ -12,10 +12,10 @@ dotenv.config();
 export const Authentication = (req, res) => {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
-    if (token === undefined) return responseHelper(res, status.errorToken, message.tokenNotFound);
+    if (token === undefined) return responseHelper(res, status.errorRequest, message.tokenNotFound);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
-        if (error) return responseHelper(res, status.errorRequest, message.errorVerifyToken);
+        if (error) return responseHelper(res, status.errorAccessToken, message.errorVerifyToken);
         req.user = user;
     });
     // console.log(token);
--- src/_helpers/ResponseHelper.js
+++ src/_helpers/ResponseHelper.js
@@ -1,12 +1,12 @@
 export const message = {
     errorServer: "Something went wrong, please try again later !!",
 
-    tokenNotFound: "Please include your token",
+    tokenNotFound: "Access token required !!!",
     tokenValid: "Token valid",
 
     errorVerifyToken: "Something wrong when verifying token, please try again later !!!",
     errorGeneratingToken: "Something wrong when generating token, please try again later !!!",
-    errorVerifyToken: "Token invalid, please request again !!!",
+    errorVerifyToken: "Access token invalid, please request again !!!",
     errorGeneratingToken: "Something wrong when generating token, please try again later !!!",
 
     successAddingMedicine: "New Medicine added successfully",
@@ -17,11 +17,11 @@ export const message = {
 export const status = {
     success: 200,
     successCreateNewData: 201,
-    notValidIfExist: 403,
+    errorRefreshToken: 403, // Forbidden
     validIfNotExist: 202,
     errorServer: 500, //data not saved in DB, or other server error
-    errorRequest: 400,
-    errorToken: 401, // check if user not passing the token
+    errorRequest: 400, // Bad Request
+    errorAccessToken: 401, // Unauthorized
     notFound: 404,
 };
 
